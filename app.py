dc37abf56b397d5d7fe5ac49e2572336a0c86442
Teemu Kuutti <teemusta@gmail.com>
2026-01-28 14:10:06 +0200

    Add matplotlib chart to Streamlit app

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 8, 6, 7, 9]
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title("My First Chart")
+ax.set_xlabel("X axis")
+ax.set_ylabel("Y axis")
+
+st.pyplot(fig)
+
+
 st.title("week 1 assignment")
 st.write("By Teemu Kuutti")
 
@@ -11,6 +25,15 @@ data = pd.DataFrame(
     columns=["A", "B", "C"]
 )
 
-# Display chart
-st.line_chart(data)
+st.title("week 2 assignment")
+st.write("By Teemu Kuutti")
+x = [1, 2, 3, 4, 5]
+y = [10, 8, 6, 7, 9]
 
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title("My First Chart")
+ax.set_xlabel("X axis")
+ax.set_ylabel("Y axis")
+
+st.pyplot(fig)
